88f5a7247bf6737ad83cc41dd125c54aeb8e88cd
Irfan Khan <irfan@infopower.co.nz>
2024-01-19 07:51:43 +1100

    fix: updated typescripts

--- src/store/storeProvider.tsx
+++ src/store/storeProvider.tsx
@@ -1,23 +1,21 @@
-import React, { createContext, useContext } from 'react';
-import { store$ } from './store';
+import React, { createContext, useContext } from "react";
+import { store$ } from "./store";
 
 const StoreContext = createContext({});
 
 // we can wrap the APP with this store provider.
-export const StoreProvider = ({ children }) => {
-    return (
-        <StoreContext.Provider value={store$}>
-            {children}
-        </StoreContext.Provider>
-    );
+export const StoreProvider = ({ children }: any) => {
+  return (
+    <StoreContext.Provider value={store$}>{children}</StoreContext.Provider>
+  );
 };
 
 export const useStoreContext = () => {
-    const context = useContext(StoreContext);
+  const context = useContext(StoreContext);
 
-    if (!context) {
-        throw new Error('useStoreContext must be used within a StoreProvider');
-    }
+  if (!context) {
+    throw new Error("useStoreContext must be used within a StoreProvider");
+  }
 
-    return context;
+  return context;
 };
