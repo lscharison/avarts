466e6b6800f0be4e1c998273c21d009903ac4a9d
Irfan Khan <irfan@infopower.co.nz>
2024-02-05 23:50:53 +1100

    fix: pages section updated

--- src/components/editor/page-title.tsx
+++ src/components/editor/page-title.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { deckPageConfigs } from "@/constants/pages";
 import { Typography } from "@material-tailwind/react";
+import { useEditorPagesObserveable } from "@/hooks/useEditorPagesObserveable";
+import { filter, first } from "lodash";
+import { PageTypes } from "@/types/editor.types";
 
 type PageTitleProps = {
   page: number;
 };
 
 export const PageTitle = ({ page }: PageTitleProps) => {
-  const pageInfo = deckPageConfigs.find((p) => p.pageIndex === page);
+  // const pageInfo = deckPageConfigs.find((p) => p.pageIndex === page);
+  const pages$ = useEditorPagesObserveable();
+  const pageInfo = first(
+    filter(pages$, (pageValue: PageTypes) => pageValue.pageNumber === page)
+  );
+
   return (
     <div className="flex justify-center items-center">
       <Typography
@@ -17,7 +24,7 @@ export const PageTitle = ({ page }: PageTitleProps) => {
           fontFamily: "inherit",
         }}
       >
-        {pageInfo?.title}
+        {pageInfo ? pageInfo.name || pageInfo?.title : ""}
       </Typography>
     </div>
   );
