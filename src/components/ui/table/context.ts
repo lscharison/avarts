18882878fb3bcaa02ab109bb51ae028e462ccaee
Irfan Khan <irfan@infopower.co.nz>
2024-02-23 22:26:25 +1100

    fix: updated view layouts

--- src/components/ui/table/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { t_table } from "./types";
-import { createTable } from "@tanstack/react-table";
-
-export const TableContext = createContext<{
-  table?: t_table<any>;
-}>({
-  table: undefined,
-});
--- src/components/ui/table/types.ts
+++
@@ -1,3 +0,0 @@
-import { Table } from "@tanstack/react-table";
-
-export type t_table<T> = Table<T>;
