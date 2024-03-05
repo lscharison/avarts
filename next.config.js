449a19f21f636036e6abfbc95032e45262edfb62
Irfan Khan <irfan@infopower.co.nz>
2024-03-06 07:36:47 +1100

    fix: updated loaders

--- next.config.js
+++ next.config.js
@@ -9,10 +9,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
-    config.module.rules.push({
-      test: /\.node/,
-      use: "raw-loader",
-    });
     return config;
   },
 };
