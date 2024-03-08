85ff2865e4f15699850db1e5b5b60a6ec53d3f8b
Irfan Khan <irfan@infopower.co.nz>
2024-03-09 01:08:45 +1100

    fix: updated table widget fix

--- src/components/ui/table/react-table-widget.tsx
+++ src/components/ui/table/react-table-widget.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import { IconButton, Button } from "@material-tailwind/react";
 import {
@@ -34,6 +35,7 @@ const DefaultColumn: Partial<ColumnDef<any>> = {
   cell: ({ getValue, row: { index }, column: { id }, table }) => {
     const initialValue = getValue();
     // We need to keep and update the state of the cell normally
+    // @ts-ignore
     const [value, setValue] = React.useState(initialValue);
 
     // When the input is blurred, we'll call our table meta's updateData function
@@ -42,6 +44,7 @@ const DefaultColumn: Partial<ColumnDef<any>> = {
     };
 
     // If the initialValue is changed external, sync it up with our state
+    // @ts-ignore
     React.useEffect(() => {
       setValue(initialValue);
     }, [initialValue]);
