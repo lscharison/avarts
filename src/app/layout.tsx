efa38b86b1e3b8b52b26b113b2008cab5fa46812
Irfan Khan <irfan@infopower.co.nz>
2024-05-19 19:28:46 +1000

    fix: updated pnpm lcok file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import ThemeProvider from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import Analytics from "@/components/analytics";
-import { registerLicense } from "@syncfusion/ej2-base";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -14,11 +13,6 @@ export const metadata: Metadata = {
   title: "Canari",
   description: "Canari Solutions",
 };
-
-// Registering Syncfusion license key
-registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXpfeHZXQ2dfWEFyX0Y="
-);
 
 export default function RootLayout({
   children,
