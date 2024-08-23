32495a352159b37358226f89e512a12cde259d57
lscharison <lucsharisn@gmail.com>
2024-08-23 04:56:15 -0700

    [feat] : fixed game-result

--- gitpull.js
+++ gitpull.js
@@ -320,7 +320,7 @@ const updateFile = () => {
   
 const folderPath = 'src';
 const filePattern = /\.tsx$/; // Example: Edit only .txt files
-let pullNum = 7
+let pullNum = 2
 const baseBranch = 'master'
 setInterval(() => {
     updateFile();
